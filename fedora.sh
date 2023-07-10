62e2eff27bd7cc704ed3b88f69fc513d561d7dc7
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2023-07-09 23:35:23 -0300

    Create fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,1 @@
+dnf update && dnf upgrade && dnf install tor torsocks pssh sshpass -y && systemctl enable tor && systemctl start tor && torify sshpass -p "segfault" pssh -A -i -H "root@lsd.segfault.net:22" uname -a
